ae9a24ea767cc604d4dbea4b66442f90c4bf1a9b
gralkapk <patrick.gralka@informatik.uni-stuttgart.de>
2025-04-07 15:10:01 +0200

    Benchmark and power measurement functionality

--- src/core/Core.cpp
+++ src/core/Core.cpp
@@ -1,11 +1,13 @@
 #include "Core.h"
 
 #include <algorithm>
+#include <chrono>
 #include <cstddef>
 #include <iomanip>
 #include <iostream>
 #include <sstream>
 #include <stdexcept>
+#include <thread>
 #include <utility>
 
 #include <glad/gl.h>
@@ -20,6 +22,82 @@
 #include "util/GLFWUtil.h"
 #include "util/GLUtil.h"
 #include "util/ImageUtil.h"
+
+#ifdef OGL4CORE2_ENABLE_PWROWG
+#include <fstream>
+#include <visus/pwrowg/csv_column.h>
+#include <visus/pwrowg/csv_iomanip.h>
+#include <visus/pwrowg/csv_sink.h>
+#include <visus/pwrowg/emi_configuration.h>
+#include <visus/pwrowg/hmc8015_configuration.h>
+#include <visus/pwrowg/msr_configuration.h>
+#include <visus/pwrowg/nvml_configuration.h>
+#include <visus/pwrowg/sensor_array.h>
+#include <visus/pwrowg/sensor_array_configuration.h>
+#include <visus/pwrowg/sensor_filters.h>
+#include <visus/pwrowg/tinkerforge_configuration.h>
+
+/**
+* Convert a wide char string to std::string.
+* @param name Wide char string.
+* @return Char string.
+*/
+std::string unmueller_string(wchar_t const* name) {
+    // https://en.cppreference.com/w/cpp/locale/codecvt/out
+
+    auto const& f = std::use_facet<std::codecvt<wchar_t, char, std::mbstate_t>>(std::locale());
+
+    std::wstring internal(name);
+    std::mbstate_t mb = std::mbstate_t();
+    std::string external(internal.size() * f.max_length(), '\0');
+    const wchar_t* from_next;
+    char* to_next;
+
+    auto const res = f.out(mb, &internal[0], &internal[internal.size()], from_next, &external[0],
+        &external[external.size()], to_next);
+    if (res != std::codecvt_base::ok) {
+        throw std::runtime_error("could not convert string");
+    }
+    external.resize(to_next - &external[0]);
+
+    return external;
+}
+
+using stream_type = std::ofstream;
+using sink_type = visus::pwrowg::atomic_sink<visus::pwrowg::csv_sink<stream_type>>;
+
+struct pwr_owg_config_t {
+    visus::pwrowg::sensor_array sensors;
+
+    std::vector<std::vector<std::pair<std::uint64_t, float>>> sensor_buffers;
+    std::vector<std::string> sensor_names;
+
+    std::unique_ptr<sink_type> sink;
+};
+
+static void sample_func(visus::pwrowg::sample const* samples, std::size_t cnt,
+    visus::pwrowg::sensor_description const* sensors, void* ctx) {
+    auto& bufs = reinterpret_cast<pwr_owg_config_t*>(ctx)->sensor_buffers;
+    for (std::size_t i = 0; i < cnt; ++i) {
+        bufs[samples[i].source].push_back(
+            std::pair<uint64_t, float>{samples[i].timestamp.value(), samples[i].reading.floating_point});
+    }
+}
+
+std::unique_ptr<sink_type> generate_sink(std::filesystem::path const& filepath) {
+    stream_type stream;
+    stream.open(filepath);
+    if (!stream.is_open()) {
+        std::cout << "failed to open output stream" << std::endl;
+    }
+    stream << visus::pwrowg::setcsvdelimiter(';');
+    stream << visus::pwrowg::setcsvcolumns(
+        visus::pwrowg::csv_column::id | visus::pwrowg::csv_column::label | visus::pwrowg::csv_column::name);
+    visus::pwrowg::csvheader(stream);
+
+    return std::make_unique<sink_type>(std::chrono::milliseconds(10), std::move(stream));
+}
+#endif
 
 using namespace OGL4Core2::Core;
 
@@ -45,7 +123,80 @@ Core::Core(Config cfg)
       contentScale_(-1.0f),
       mouseX_(0.0),
       mouseY_(0.0),
-      cameraControlMode_(AbstractCamera::MouseControlMode::None) {
+      cameraControlMode_(AbstractCamera::MouseControlMode::None)
+#ifdef OGL4CORE2_ENABLE_PWROWG
+      ,
+      window_size_{initWindowSizeWidth, initWindowSizeHeight},
+      old_window_size_{0, 0}
+#endif
+{
+#ifdef OGL4CORE2_ENABLE_PWROWG
+    try {
+        pwr_owg_config_ = new pwr_owg_config_t;
+
+        reinterpret_cast<pwr_owg_config_t*>(pwr_owg_config_)->sink = generate_sink("./pwr.csv");
+
+        visus::pwrowg::sensor_array_configuration config;
+        config.sample_every(std::chrono::milliseconds(sample_interval_))
+            .configure<visus::pwrowg::tinkerforge_configuration>([](visus::pwrowg::tinkerforge_configuration& c) {
+                c.averaging(visus::pwrowg::tinkerforge_sample_averaging::average_of_4)
+                    .voltage_conversion_time(visus::pwrowg::tinkerforge_conversion_time::milliseconds_1_1)
+                    .current_conversion_time(visus::pwrowg::tinkerforge_conversion_time::milliseconds_1_1);
+            })
+            .configure<visus::pwrowg::nvml_configuration>([](visus::pwrowg::nvml_configuration& c) {})
+            .exclude<visus::pwrowg::hmc8015_configuration>()
+            .exclude<visus::pwrowg::emi_configuration>()
+            .exclude<visus::pwrowg::msr_configuration>()
+            .deliver_context(reinterpret_cast<pwr_owg_config_t*>(pwr_owg_config_)->sink.get())
+            .deliver_to(sink_type::sample_callback);
+
+        reinterpret_cast<pwr_owg_config_t*>(pwr_owg_config_)->sensors =
+            visus::pwrowg::sensor_array::for_matches(std::move(config), visus::pwrowg::is_power_sensor);
+
+        auto& sensors = reinterpret_cast<pwr_owg_config_t*>(pwr_owg_config_)->sensors;
+        auto& names = reinterpret_cast<pwr_owg_config_t*>(pwr_owg_config_)->sensor_names;
+        names.clear();
+        names.reserve(sensors.size());
+
+        for (int i = 0; i < sensors.size(); ++i) {
+            auto& s = sensors[i];
+            if (visus::pwrowg::is_tinkerforge_sensor(s)) {
+                if (unmueller_string(s.path()) == "UgH") {
+                    s.label("HPWR0");
+                }
+                if (unmueller_string(s.path()) == "Ugu") {
+                    s.label("HPWR1");
+                }
+                if (unmueller_string(s.path()) == "Ufm") {
+                    s.label("HPWR2");
+                }
+                if (unmueller_string(s.path()) == "UgF") {
+                    s.label("HPWR3");
+                }
+                if (unmueller_string(s.path()) == "Uft") {
+                    s.label("HPWR4");
+                }
+                if (unmueller_string(s.path()) == "UeW") {
+                    s.label("HPWR5");
+                }
+                if (unmueller_string(s.path()) == "UfN") {
+                    s.label("PEG3V");
+                }
+                if (unmueller_string(s.path()) == "U6Q") {
+                    s.label("PEG5V");
+                }
+            }
+            names.push_back(unmueller_string(s.id()) + "%" + unmueller_string(s.name()));
+        }
+    } catch (...) {
+        std::cout << "failed to configure PWROWG" << std::endl;
+        if (pwr_owg_config_) {
+            delete pwr_owg_config_;
+            pwr_owg_config_ = nullptr;
+        }
+    }
+#endif
+
     Core::initGLFW();
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, openGLVersionMajor);
@@ -179,6 +330,13 @@ Core::~Core() {
 
     glfwDestroyWindow(window_);
     Core::terminateGLFW();
+
+#ifdef OGL4CORE2_ENABLE_PWROWG
+    reinterpret_cast<pwr_owg_config_t*>(pwr_owg_config_)->sink->dispose();
+    if (pwr_owg_config_) {
+        delete pwr_owg_config_;
+    }
+#endif
 }
 
 void Core::run() {
@@ -186,7 +344,7 @@ void Core::run() {
         throw std::runtime_error("Core is already running!");
     }
     running_ = true;
-    while (!glfwWindowShouldClose(window_)) {
+    auto render_frame = [&]() {
         frameNumber_++;
 
         if (fps_.tick()) {
@@ -200,6 +358,70 @@ void Core::run() {
 
         glfwSwapBuffers(window_);
         glfwPollEvents();
+    };
+    while (!glfwWindowShouldClose(window_)) {
+#ifdef OGL4CORE2_ENABLE_PWROWG
+        if (recording_paths_) {
+            recorded_entries_.push_back(current_entry_);
+            current_entry_.newX = current_entry_.oldX;
+            current_entry_.newY = current_entry_.oldY;
+            current_entry_.xoffset = 0;
+            current_entry_.yoffset = 0;
+        }
+        if (run_benchmark_) {
+            try {
+                std::cout << "started benchmark" << std::endl;
+                reinterpret_cast<pwr_owg_config_t*>(pwr_owg_config_)->sink = generate_sink(output_file_);
+                reinterpret_cast<pwr_owg_config_t*>(pwr_owg_config_)
+                    ->sensors.start(sink_type::sample_callback,
+                        reinterpret_cast<pwr_owg_config_t*>(pwr_owg_config_)->sink.get());
+                auto iter_time = std::chrono::steady_clock::now();
+                auto const start = std::chrono::steady_clock::now();
+                if (replay_paths_) {
+                    for (std::size_t i = 0; i < recorded_entries_.size(); ++i) {
+                        auto const& ce = recorded_entries_[i];
+                        if (!recorded_gaze_points_.empty()) {
+                            current_gaze_point_ = recorded_gaze_points_[i];
+                        }
+                        auto camera = camera_.lock();
+                        if (camera) {
+                            camera->mouseMoveControl(ce.mode, ce.oldX, ce.oldY, ce.newX, ce.newY);
+                            camera->mouseScrollControl(ce.xoffset, ce.yoffset);
+                        }
+                        render_frame();
+                        if (use_frame_cap_) {
+                            std::this_thread::sleep_until(iter_time + std::chrono::milliseconds(frame_cap_ms_));
+                            iter_time = std::chrono::steady_clock::now();
+                        }
+                    }
+                    replay_paths_ = false;
+                } else {
+                    for (int i = 0; i < num_frames_; ++i) {
+                        render_frame();
+                        if (use_frame_cap_) {
+                            std::this_thread::sleep_until(iter_time + std::chrono::milliseconds(frame_cap_ms_));
+                            iter_time = std::chrono::steady_clock::now();
+                        }
+                    }
+                }
+                auto const end = std::chrono::steady_clock::now();
+                std::cout << "time: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()
+                          << " num frames: " << recorded_entries_.size() << std::endl;
+                reinterpret_cast<pwr_owg_config_t*>(pwr_owg_config_)->sensors.stop();
+                run_benchmark_ = false;
+                auto const& bufs = reinterpret_cast<pwr_owg_config_t*>(pwr_owg_config_)->sensor_buffers;
+                if (std::any_of(bufs.begin(), bufs.end(),
+                        [&](auto const& b) { return b.size() >= expected_samples_num_; })) {
+                    std::cout << "exceded sample buffer allocation" << std::endl;
+                }
+                std::cout << "finished benchmark" << std::endl;
+            } catch (...) {
+                std::cout << "benchmark failed" << std::endl;
+                run_benchmark_ = false;
+            }
+        }
+#endif
+        render_frame();
     }
     running_ = false;
 }
@@ -235,6 +457,24 @@ void Core::registerCamera(const std::shared_ptr<AbstractCamera>& camera) const {
 void Core::removeCamera() const {
     camera_.reset();
 }
+
+#ifdef OGL4CORE2_ENABLE_PWROWG
+void OGL4Core2::Core::Core::push_gaze_point(std::array<float, 2> const& gp) {
+    recorded_gaze_points_.push_back(gp);
+}
+
+std::array<float, 2> OGL4Core2::Core::Core::pull_gaze_point() const {
+    return current_gaze_point_;
+}
+
+bool OGL4Core2::Core::Core::record_gaze_point() const {
+    return recording_paths_;
+}
+
+bool OGL4Core2::Core::Core::replay_gaze_point() const {
+    return run_benchmark_ && replay_eyes_;
+}
+#endif
 
 void Core::validateImGuiScale() {
     float xscale, yscale;
@@ -283,6 +523,98 @@ void Core::draw() {
     if (ImGui::CollapsingHeader("Plugins", ImGuiTreeNodeFlags_DefaultOpen)) {
         ImGui::Combo("Plugin", &pluginSelectionIdx_, pluginNamesImGui_.data());
     }
+#ifdef OGL4CORE2_ENABLE_PWROWG
+    if (ImGui::CollapsingHeader("PwrOwg", ImGuiTreeNodeFlags_DefaultOpen)) {
+        ImGui::InputInt2("Window Size", window_size_.data(),
+            ImGuiInputTextFlags_::ImGuiInputTextFlags_EnterReturnsTrue);
+        if (window_size_[0] != old_window_size_[0] || window_size_[1] != old_window_size_[1]) {
+            setWindowSize(window_size_[0], window_size_[1]);
+            old_window_size_ = window_size_;
+        }
+        ImGui::InputInt("Frame Cap [ms]", &frame_cap_ms_, 1, 100,
+            ImGuiInputTextFlags_::ImGuiInputTextFlags_EnterReturnsTrue);
+        ImGui::SameLine();
+        ImGui::Checkbox("Use Frame Cap", &use_frame_cap_);
+        ImGui::SliderInt("Num Frames", &num_frames_, 1, 10000);
+        char outPath[256];
+        sprintf_s(outPath, "%s", output_file_.c_str());
+        ImGui::InputText("Output Path", outPath, 256);
+        output_file_ = std::string(outPath);
+        if (ImGui::Button("Start Benchmark")) {
+            auto const& sensors = reinterpret_cast<pwr_owg_config_t*>(pwr_owg_config_)->sensors;
+            auto& bufs = reinterpret_cast<pwr_owg_config_t*>(pwr_owg_config_)->sensor_buffers;
+            bufs.clear();
+            bufs.resize(sensors.size());
+
+            expected_samples_num_ = num_frames_ * 2 / sample_interval_; //< expecting 2ms per frame
+            std::cout << "reserving for " << expected_samples_num_ << " samples" << std::endl;
+
+            for (auto& b : bufs) {
+                b.reserve(expected_samples_num_);
+            }
+
+            run_benchmark_ = true;
+        }
+        if (ImGui::CollapsingHeader("Automation", ImGuiTreeNodeFlags_DefaultOpen)) {
+            char camPath[256];
+            sprintf_s(camPath, "%s", camera_path_file_.c_str());
+            ImGui::InputText("Cam Path", camPath, 256);
+            camera_path_file_ = std::string(camPath);
+            char eyePath[256];
+            sprintf_s(eyePath, "%s", eye_path_file_.c_str());
+            ImGui::InputText("Eye Path", eyePath, 256);
+            eye_path_file_ = std::string(eyePath);
+            if (ImGui::Checkbox("Replay", &replay_paths_)) {
+                if (std::filesystem::exists(camera_path_file_)) {
+                    auto const file_size = std::filesystem::file_size(camera_path_file_);
+                    recorded_entries_.clear();
+                    recorded_entries_.resize(file_size / sizeof(decltype(recorded_entries_)::value_type));
+                    auto cam_file = std::ifstream(std::filesystem::path(camera_path_file_), std::ios::binary);
+                    cam_file.read(reinterpret_cast<char*>(recorded_entries_.data()), file_size);
+                    cam_file.close();
+                    recorded_gaze_points_.clear();
+                    if (std::filesystem::exists(eye_path_file_)) {
+                        auto const eye_file_size = std::filesystem::file_size(eye_path_file_);
+                        recorded_gaze_points_.resize(
+                            eye_file_size / sizeof(decltype(recorded_gaze_points_)::value_type));
+                        auto eye_file = std::ifstream(std::filesystem::path(eye_path_file_), std::ios::binary);
+                        eye_file.read(reinterpret_cast<char*>(recorded_gaze_points_.data()), eye_file_size);
+                        eye_file.close();
+                        if (recorded_gaze_points_.size() != recorded_entries_.size()) {
+                            recorded_gaze_points_.clear();
+                            current_gaze_point_ = {0.5f, 0.5f};
+                        }
+                    }
+                } else {
+                    replay_paths_ = false;
+                    current_gaze_point_ = {0.5f, 0.5f};
+                }
+            }
+            ImGui::SameLine();
+            ImGui::Checkbox("Replay Eyes", &replay_eyes_);
+            if (ImGui::Button("Record")) {
+                recording_paths_ = !recording_paths_;
+                if (recording_paths_) {
+                    recorded_entries_.clear();
+                    recorded_entries_.reserve(100000);
+                    recorded_gaze_points_.clear();
+                    recorded_gaze_points_.reserve(100000);
+                } else {
+                    auto cam_file = std::ofstream(std::filesystem::path(camera_path_file_), std::ios::binary);
+                    cam_file.write(reinterpret_cast<char const*>(recorded_entries_.data()),
+                        recorded_entries_.size() * sizeof(decltype(recorded_entries_)::value_type));
+                    cam_file.close();
+                    auto eye_file = std::ofstream(std::filesystem::path(eye_path_file_), std::ios::binary);
+                    eye_file.write(reinterpret_cast<char const*>(recorded_gaze_points_.data()),
+                        recorded_gaze_points_.size() * sizeof(decltype(recorded_gaze_points_)::value_type));
+                    eye_file.close();
+                }
+            }
+            ImGui::SameLine();
+            ImGui::Checkbox("Recording", &recording_paths_);
+        }
+    }
+#endif
     if (currentPluginIdx_ != pluginSelectionIdx_) {
         currentPluginIdx_ = pluginSelectionIdx_;
         // Need to delete plugin first, so destructor of old plugin runs before constructor of new plugin.
@@ -405,6 +737,15 @@ void Core::mouseMoveEvent(double xpos, double ypos) {
                 double newX = 2.0 * xpos / static_cast<double>(framebufferWidth_) - 1.0;
                 double newY = 1.0 - 2.0 * ypos / static_cast<double>(framebufferHeight_);
                 camera->mouseMoveControl(cameraControlMode_, oldX, oldY, newX, newY);
+#ifdef OGL4CORE2_ENABLE_PWROWG
+                if (recording_paths_) {
+                    current_entry_.mode = cameraControlMode_;
+                    current_entry_.oldX = oldX;
+                    current_entry_.oldY = oldY;
+                    current_entry_.newX = newX;
+                    current_entry_.newY = newY;
+                }
+#endif
             }
         }
 
@@ -420,6 +761,12 @@ void Core::mouseScrollEvent(double xoffset, double yoffset) {
             auto camera = camera_.lock();
             if (camera) {
                 camera->mouseScrollControl(xoffset, yoffset);
+#ifdef OGL4CORE2_ENABLE_PWROWG
+                if (recording_paths_) {
+                    current_entry_.xoffset = xoffset;
+                    current_entry_.yoffset = yoffset;
+                }
+#endif
             }
         }
         currentPlugin_->mouseScroll(xoffset, yoffset);
--- src/core/Core.h
+++ src/core/Core.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <array>
 #include <cstdint>
 #include <exception>
 #include <filesystem>
@@ -43,6 +44,13 @@ namespace OGL4Core2::Core {
 
         void registerCamera(const std::shared_ptr<AbstractCamera>& camera) const;
         void removeCamera() const;
+
+#ifdef OGL4CORE2_ENABLE_PWROWG
+        void push_gaze_point(std::array<float, 2> const& gp);
+        std::array<float,2> pull_gaze_point() const;
+        bool record_gaze_point() const;
+        bool replay_gaze_point() const;
+#endif
 
     private:
         void validateImGuiScale();
@@ -95,5 +103,32 @@ namespace OGL4Core2::Core {
         static void terminateGLFW();
 
         static int glfwReferenceCounter_;
+
+#ifdef OGL4CORE2_ENABLE_PWROWG
+        bool run_benchmark_ = false;
+        int num_frames_ = 100;
+        int expected_samples_num_ = 1;
+        int sample_interval_ = 10;
+        void* pwr_owg_config_ = nullptr;
+        std::string output_file_ = "./pwr.csv";
+        std::string camera_path_file_ = "./cam.path";
+        std::string eye_path_file_ = "./eye.path";
+        bool replay_paths_ = false;
+        bool replay_eyes_ = false;
+        bool recording_paths_ = false;
+        std::array<int, 2> window_size_;
+        std::array<int, 2> old_window_size_;
+        int frame_cap_ms_ = 30;
+        bool use_frame_cap_ = false;
+
+        struct cam_control_entry {
+            AbstractCamera::MouseControlMode mode;
+            double oldX, oldY, newX, newY, xoffset, yoffset;
+        };
+        cam_control_entry current_entry_;
+        std::vector<cam_control_entry> recorded_entries_;
+        std::vector<std::array<float, 2>> recorded_gaze_points_;
+        std::array<float, 2> current_gaze_point_ = {0.5f, 0.5f};
+#endif
     };
 } // namespace OGL4Core2::Core
